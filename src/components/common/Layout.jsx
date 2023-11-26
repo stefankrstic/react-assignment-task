5fd5224d76b97e89cb5569051cc341d5bd9615e9
stefankrstic <stefan.k.krstic@gmail.com>
2023-11-26 22:50:02 +0100

    Move repeating head component to layout

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,9 +1,17 @@
 import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
+import Head from "next/head";
 import Link from "next/link";
 
 export const Layout = ({ children }) => {
     return (
         <>
+            <Head>
+                <title>React Assignment Task</title>
+                <meta name="description" content="Simple app for managing roles and users" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
             <AppBar position="static" component="nav">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
